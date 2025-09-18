d3d450565fe03e9ab13e7042da8ebb49f2498554
Agnes Au <ckaau@connect.ust.hk>
2025-09-19 00:07:57 +0800

    feat: movement
    things are still very buggy
    - player is moving 2 tiles at a time for some reason
    - you can keep moving if you hold down the key

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -10,16 +10,19 @@ var letters = []
 var player
 var LETTER_SPRITE_SCENE = preload("res://scenes/Letter.tscn")
 
+
+func _ready():
+	player = $Player
+	player.input_received.connect(_on_input_received)
+	generate_grid()
+	position_grid()
+	set_player_input()
+
 func new_letter(letter):
 	var letter_sprite = LETTER_SPRITE_SCENE.instantiate()
 	add_child(letter_sprite)
 	letter_sprite.animation = letter
 	return letter_sprite
-
-func _ready():
-	player = $Player
-	generate_grid()
-	position_grid()
 	
 func generate_grid():
 	for i in range(GRID_SIZE):
@@ -35,3 +38,25 @@ func position_grid():
 			var letter_cell = new_letter(letters[i][j])
 			@warning_ignore("integer_division")
 			letter_cell.position = Vector2(i * TILE_SIZE + TILE_SIZE/2, j * TILE_SIZE + TILE_SIZE/2)
+
+func set_player_input():
+	var player_pos_coord = Vector2((player.position.x/32-1)/2, (player.position.y/32-1)/2)
+	var player_x = player_pos_coord.x
+	var player_y = player_pos_coord.y
+	var right_coord = Vector2(player_x+1, player_y)
+	var left_coord = Vector2(player_x-1, player_y)
+	var up_coord = Vector2(player_x, player_y-1)
+	var down_coord = Vector2(player_x, player_y+1)
+	var new_inputs = []
+	
+	for coord in [right_coord, left_coord, up_coord, down_coord]:
+		if coord.x < 0 or coord.x >= 8 or coord.y < 0 or coord.y >= 8:
+			new_inputs.append(null)
+		else:
+			new_inputs.append(letters[coord.x][coord.y])
+			
+	player.set_inputs(new_inputs)
+
+func _on_input_received():
+	set_player_input()
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,17 +7,28 @@ var inputs = {
 	"ui_up": Vector2.UP,
 	"ui_down": Vector2.DOWN
 }
+signal input_received(letter)
 	
 func _ready():
 	position = position.snapped(Vector2.ONE * TILE_SIZE)
 	position += Vector2.ONE * TILE_SIZE/2
 
 func _unhandled_input(event: InputEvent) -> void:
+	if event.as_text() in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" and event.is_pressed() and not event.echo:
+		input_received.emit()
+
 	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
+		if event.as_text() == dir:
 			move(dir)
 			
 func move(dir):
 	position += inputs[dir] * TILE_SIZE
 	print(position)
-	
+
+func set_inputs(new_inputs):
+	inputs = {
+		new_inputs[0]: Vector2.RIGHT,
+		new_inputs[1]: Vector2.LEFT,
+		new_inputs[2]: Vector2.UP,
+		new_inputs[3]: Vector2.DOWN
+	}
